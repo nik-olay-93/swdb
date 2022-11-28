eca1da090a81e635c12eff327d301bb288d60def
Nikolay Yaroshenko <nik.olay.93@mail.ru>
2022-11-28 03:33:30 +0300

    auth setup

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -15,6 +15,10 @@ import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/signup" page={SignupPage} name="signup" />
+      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
       <Set wrap={GroupsMenuLayout}>
         <Route path="/" page={HomePage} name="home" />
 
